91ac16aa4efbfc09d371d36950bde3824ebd79fb
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-03-28 09:01:11 -0400

    Style reset button and further style sketch area

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,9 @@ const sketch = document.querySelector('#sketch-area');
 const desiredWidth = 40;
 sketch.style.width = `${desiredWidth}vw`;
 
+const resetButton = document.querySelector('#reset');
+resetButton.style.fontSize = `${desiredWidth / 20}vw`;
+
 const numPixels = 16;
 for (let i = 0; i < numPixels ** 2; i++) {
   const newPixel = document.createElement('div');
@@ -12,13 +15,6 @@ for (let i = 0; i < numPixels ** 2; i++) {
 
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach(pixel => pixel.addEventListener('mouseover', fill.bind(null, pixel)))
-
-//Alternate code for adding the event listener to each pixel
-/*
-pixels.forEach(pixel => pixel.addEventListener('mouseover', () => {
-  pixel.classList.add('filled');
-}));
-*/
 
 function fill (box) {
   box.classList.add('filled');
