bd4f98a94691a05fe08ae26d4bf959ed296ab369
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-03-28 09:48:48 -0400

    Add functionality to reset button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,18 +4,27 @@ sketch.style.width = `${desiredWidth}vw`;
 
 const resetButton = document.querySelector('#reset');
 resetButton.style.fontSize = `${desiredWidth / 20}vw`;
+resetButton.addEventListener('click', clearSketch);
 
 const numPixels = 16;
 for (let i = 0; i < numPixels ** 2; i++) {
   const newPixel = document.createElement('div');
   newPixel.classList.add('pixel');
   newPixel.style.width = `${desiredWidth / numPixels}vw`; 
+  newPixel.addEventListener('mouseover',fill.bind(null, newPixel));
   sketch.appendChild(newPixel);
 }
 
+/*
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach(pixel => pixel.addEventListener('mouseover', fill.bind(null, pixel)))
+*/
 
 function fill (box) {
   box.classList.add('filled');
+}
+
+function clearSketch () {
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pixel => pixel.classList.remove('filled'));
 }
